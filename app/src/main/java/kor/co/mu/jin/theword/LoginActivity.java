06608deef723df7e71b335cbdc699e9c019bf809
MJin <jinwithgame@gmail.com>
2020-07-08 16:47:41 +0900

    로그인 액티비티 완성
    TODO : 로그인 완료시 다이얼로그 화면 완성

--- app/src/main/java/kor/co/mu/jin/theword/LoginActivity.java
+++ app/src/main/java/kor/co/mu/jin/theword/LoginActivity.java
@@ -1,15 +1,34 @@
 package kor.co.mu.jin.theword;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.kakao.auth.ISessionCallback;
 import com.kakao.auth.Session;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.MeV2ResponseCallback;
+import com.kakao.usermgmt.response.MeV2Response;
+import com.kakao.usermgmt.response.model.Profile;
+import com.kakao.usermgmt.response.model.UserAccount;
 import com.kakao.util.exception.KakaoException;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class LoginActivity extends AppCompatActivity {
+
+    CircleImageView pimg;
+    TextView pnickname;
+    TextView pmessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +42,9 @@ public class LoginActivity extends AppCompatActivity {
         @Override
         public void onSessionOpened() {
             Toast.makeText(LoginActivity.this, "로그인 성공", Toast.LENGTH_SHORT).show();
+            LayoutInflater inflater = getLayoutInflater();
+            View view = inflater.inflate(R.layout.whenlogincomplete, null);
+            new AlertDialog.Builder(LoginActivity.this).setView(view).create().show();
         }
 
         @Override
@@ -36,4 +58,18 @@ public class LoginActivity extends AppCompatActivity {
         super.onDestroy();
         Session.getCurrentSession().removeCallback(sessionCallback);
     }
+
+    public void clickNo_login(View view) {
+
+        new AlertDialog.Builder(this).setMessage("정말 로그인 없이 진행하시겠습니까 ?").setPositiveButton("확인", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }).setNegativeButton("취소", null).create().show();
+    }
+
+
 }
