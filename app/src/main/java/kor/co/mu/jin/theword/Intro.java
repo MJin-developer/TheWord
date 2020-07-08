5208dd00a00a500edc433a37b14829c9d00b381a
MJin <jinwithgame@gmail.com>
2020-07-08 14:59:55 +0900

    로그인 액티비티 완성
    TODO : 로그인 작업

--- app/src/main/java/kor/co/mu/jin/theword/Intro.java
+++ app/src/main/java/kor/co/mu/jin/theword/Intro.java
@@ -3,14 +3,23 @@ package kor.co.mu.jin.theword;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Intro extends AppCompatActivity {
 
@@ -26,6 +35,10 @@ public class Intro extends AppCompatActivity {
         Animation animationIn = AnimationUtils.loadAnimation(this, R.anim.fadein);
         text_intro_word = findViewById(R.id.text_intro_word);
         text_intro_word.startAnimation(animationIn);
+
+        getKeyHash getKeyHash = new getKeyHash();
+        String Hash = getKeyHash.KeyHash(this);
+        Log.i("KEY HASH", Hash);
     }
 
     Handler handler = new Handler(){
---
+++ app/src/main/java/kor/co/mu/jin/theword/MyApplication.java
@@ -0,0 +1,28 @@
+package kor.co.mu.jin.theword;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.kakao.auth.IApplicationConfig;
+import com.kakao.auth.KakaoAdapter;
+import com.kakao.auth.KakaoSDK;
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // 카카오 SDK와 앱의 Application 연결
+        KakaoSDK.init(new KakaoAdapter() {
+            @Override
+            public IApplicationConfig getApplicationConfig() {
+                return new IApplicationConfig() {
+                    @Override
+                    public Context getApplicationContext() {
+                        return MyApplication.this;
+                    }
+                };
+            }
+        });
+    }
+}
